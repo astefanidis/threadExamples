b80d82e654074d592142a687dbfabb8c1c13cc9b
Apostolos Stefanidis <astefanidis94@gmail.com>
2017-06-15 12:12:53 +0300

    Final version

--- biggestElement.cpp
+++ biggestElement.cpp
@@ -2,6 +2,7 @@
 #include <thread>
 #include <vector>
 #include <cfloat>
+
 #define NUM_THREADS 8
 
 using namespace std;
@@ -21,21 +22,22 @@ void returnBiggestElement(vector<float> numbers, float* biggest)
 	*biggest = maxNum;
 }
 
+
 float maxNumber(vector<float> &numbers)
 {
 	
 	
-	int vectorElementsPerThread = numbers.size()/NUM_THREADS;
-	int remainderElements = numbers.size() % NUM_THREADS;
+	int vectorElementsPerThread = numbers.size()/NUM_THREADS;  //number of elements each thread will process
+	int remainderElements = numbers.size() % NUM_THREADS;      //will also be allocated to threads
 	int unusedThreads = 0;
-	if(vectorElementsPerThread == 0)
+	if(vectorElementsPerThread == 0)    //if we have more elements than threads, we can leave some threads unused
 	{
 		unusedThreads = NUM_THREADS - numbers.size();
 	}
-	vector<float> maxFromEachThread(NUM_THREADS-unusedThreads);
-	vector<thread> threads(NUM_THREADS-unusedThreads);
-	int start=0;
-	int end=0;
+	vector<float> maxFromEachThread(NUM_THREADS-unusedThreads);  //will store the max number from each thread
+	vector<thread> threads(NUM_THREADS-unusedThreads);           //the threads
+	int start=0;  //first element the thread will process
+	int end=0;    //last+1 element
 	
 	for(int i = 0; i<NUM_THREADS - unusedThreads; i++)
 	{
@@ -46,33 +48,31 @@ float maxNumber(vector<float> &numbers)
 		}
 		else
 		{
-			end = start + vectorElementsPerThread + 1; 
+			end = start + vectorElementsPerThread + 1; //add the remainder elements until they finish
 			remainderElements--;
 		}
-		cout << "thread: " << i << "start: " << start << " end: " << end << endl;
+		
 		for(int j=start; j<end; j++)
 		{
 			tempElements.push_back(numbers[j]);
 		}
 		
-		//thread t(returnBiggestElement,tempElements,&localMax);
+		
 		threads[i] = thread(returnBiggestElement,tempElements,&maxFromEachThread[i]);
-		//maxFromEachThread[i] = localMax;
-		//threads.push_back(move(t));
+		
 		start = end;
 	}
 	for(int i=0; i<threads.size(); i++)
 	{
-		if(true)//threads[i].joinable())
+		
+		if(threads[i].joinable())
 		{
 			threads[i].join();
 		}
 	}
-	for(auto i:maxFromEachThread)
-	{
-		cout << i << endl;
-	}
+	
 	float maxNum = -FLT_MAX;
+	//find the global maximum
 	for(auto i:maxFromEachThread)
 	{
 		if(i>maxNum)
@@ -87,10 +87,10 @@ float maxNumber(vector<float> &numbers)
 
 int main()
 {
-	vector<float> numbers(100);
+	vector<float> numbers(101);
 	for(int i=0; i<numbers.size(); i++)
 	{
-		numbers[i] = -i*i;
+		numbers[i] = i*i;
 	}
 	float maxNum = maxNumber(numbers);
 	cout << maxNum << endl;
